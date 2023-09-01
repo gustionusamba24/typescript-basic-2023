ab650da1788227aeda47b53284a3db2b14a1c811
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-09-01 10:53:18 +0700

    type alias typescript

--- tests/type-alias.test.ts
+++ tests/type-alias.test.ts
@@ -1,20 +1,20 @@
-import { Category, Product } from "../src/type-alias";
+import {Category, Product} from "../src/type-alias";
 
 describe("Alias", function () {
-  it("should support alias", function () {
-    const category: Category = {
-      id: "1",
-      name: "Handphone",
-    };
+    it("should support alias", function () {
+        const category: Category = {
+            id: "1",
+            name: "Handphone",
+        };
 
-    const product: Product = {
-      id: 23,
-      name: "Samsung S23",
-      price: 20000000,
-      category: category,
-    };
+        const product: Product = {
+            id: 23,
+            name: "Samsung S23",
+            price: 20000000,
+            category: category,
+        };
 
-    console.info(category);
-    console.info(product);
-  });
+        console.info(category);
+        console.info(product);
+    });
 });
