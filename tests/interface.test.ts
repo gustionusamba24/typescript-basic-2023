73dcb250eebe6f7d643224d1a489e9582fcd0032
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-09-03 20:54:54 +0700

    Function Interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -3,14 +3,23 @@ import {Seller} from "../src/seller";
 describe("Interface", function () {
     it("should support an interface", function () {
         const seller: Seller = {
-            id: 1,
-            name: "Aldi",
-            nib: "121379",
-            npwp: "2324242",
+            id: 1, name: "Aldi", nib: "121379", npwp: "2324242",
         };
 
         seller.name = "Taher";
 
         console.info(seller);
+    });
+
+    it('should support function interface', () => {
+        interface AddFunction {
+            (value1: number, value2: number): number;
+        }
+
+        const add: AddFunction = (value1: number, value2: number): number => {
+            return value1 + value2;
+        }
+
+        expect(add(3, 5)).toBe(8);
     });
 });
