1405cb1be43f2c3b2fa120019947ec7f6ef31e41
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-09-03 20:28:42 +0700

    Interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,16 +1,16 @@
-import { Seller } from "../src/seller";
+import {Seller} from "../src/seller";
 
 describe("Interface", function () {
-  it("should support an interface", function () {
-    const seller: Seller = {
-      id: 1,
-      name: "Aldi",
-      nib: "121379",
-      npwp: "2324242",
-    };
+    it("should support an interface", function () {
+        const seller: Seller = {
+            id: 1,
+            name: "Aldi",
+            nib: "121379",
+            npwp: "2324242",
+        };
 
-    seller.name = "Taher";
+        seller.name = "Taher";
 
-    console.info(seller);
-  });
+        console.info(seller);
+    });
 });
