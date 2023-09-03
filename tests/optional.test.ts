f676fb368c8bbe75dd2acfed788d4a67dbac4e39
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-09-03 13:27:31 +0700

    Optional Test

--- tests/optional.test.ts
+++ tests/optional.test.ts
@@ -1,16 +1,16 @@
 describe("Null safety", function () {
-  it("should ", function () {
-    function sayHello(name?: string | null) {
-      if (name) {
-        console.info(`Hello ${name}`);
-      } else {
-        console.info("Hello");
-      }
-    }
+    it("should support null and undefined", function () {
+        function sayHello(name?: string | null) {
+            if (name) {
+                console.info(`Hello ${name}`);
+            } else {
+                console.info("Hello");
+            }
+        }
 
-    sayHello("Gustio");
-    const name: string | undefined = undefined;
-    sayHello(name);
-    sayHello(null);
-  });
+        sayHello("Gustio");
+        const name: string | undefined = undefined;
+        sayHello(name);
+        sayHello(null);
+    });
 });
